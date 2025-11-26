4d1225f684283f0a868379b51ab84e6bb4b1fc41
PLGZ <mark840102@gmail.com>
2025-11-26 22:53:30 +0800

    7th

--- constants.ts
+++ constants.ts
@@ -4,7 +4,7 @@
 // 1. 建立 Google Sheet
 // 2. 擴充功能 > Apps Script > 貼上 doPost 程式碼
 // 3. 部署 > 網頁應用程式 > 誰可以存取：所有人 (Anyone)
-export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwI354Qj5uBjbsnFqBts68w8IUXTpdQXReEJbKJKJjhRztJkoO8as7F93jZYjmwfDMv/exechttps://script.google.com/macros/s/AKfycbwnPLO3ZIFPgtYxR9NNOGEiGK_O4JW1f3ut5-KeFu7-736BU88n1z8dRSYKTvZFwtopyg/exec";
+export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwI354Qj5uBjbsnFqBts68w8IUXTpdQXReEJbKJKJjhRztJkoO8as7F93jZYjmwfDMv/exec";
 
 // 這裡是用於顯示新人照片的連結
 // 如果要換照片，請將引號內的網址換成新的圖片連結 (結尾通常為 .jpg 或 .png)
